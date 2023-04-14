b83c671e0d5e033aadd2f42ee802bd5b595d4ae5
mizti <mizugokoro@gmail.com>
2023-04-14 10:57:12 +0900

    deleted temp function

--- mono/__init__.py
+++ mono/__init__.py
@@ -1,10 +1,24 @@
 import logging
 
 import azure.functions as func
+from PIL import Image
+import io
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    logging.info("---------from-----------")
+    res = "ssss"
+    logging.info("-----------end---------")
+    image_binary = ""
+    if req.method == "POST":
+        image_binary = req.get_body()
+    img = Image.open(io.BytesIO(image_binary))
+    grayscale_img = img.convert('L')
+    png_binary = io.BytesIO()
+    grayscale_img.save(png_binary, 'PNG')
+    png_binary.seek(0)
+    res = png_binary.getvalue()
 
     name = req.params.get('name')
     if not name:
@@ -19,6 +33,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+             body = res,
+             status_code=200,
+             mimetype = "image"
         )
