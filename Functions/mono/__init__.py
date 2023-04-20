314dcc27ebb2453438241ab5237e1ec81e788d68
maaya <huki.j37@gmail.com>
2023-04-20 16:31:21 +0900

    ディレクトリ構成の変更

---
+++ Functions/mono/__init__.py
@@ -0,0 +1,26 @@
+import logging
+
+import azure.functions as func
+from PIL import Image
+import io
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    image_binary = ""
+    if req.method == "POST":
+        image_binary = req.get_body()
+    img = Image.open(io.BytesIO(image_binary))
+
+    # Process image here
+    processed_img = img.convert('L')
+
+    # Save image
+    png_binary = io.BytesIO()
+    processed_img.save(png_binary, 'PNG')
+    png_binary.seek(0)
+    response = png_binary.getvalue()
+
+    return func.HttpResponse(
+            body = response,
+            status_code=200,
+            mimetype = "image"
+    )
